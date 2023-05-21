352938e67c2586205124fdb0cdf313d1f8b36f5c
briofy-mammad <m.ranjbar@briofy.net>
2023-05-21 08:41:22 +0330

    init project

---
+++ src/modules/config/index.ts
@@ -0,0 +1,10 @@
+import { IConfig } from "./interface/Config";
+
+/**
+* @description Set Base URL and other configs
+* @param endpoint: string
+* @returns your base URL is Set
+*/
+export let config: IConfig = {
+    baseURL: ''
+}
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+// vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+// https://vitejs.dev/guide/build.html#library-mode
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "my-ts",
+      fileName: "my-ts",
+    },
+  },
+  plugins: [dts()],
+});
